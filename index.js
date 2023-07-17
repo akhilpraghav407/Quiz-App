46c8b3d1fa59bbc01b9e0e8c2ba99f6f2776fb2c
akhil <akhilpraghav@gmail.com>
2023-07-17 22:40:49 +0530

    answer checked completed

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ console.log(QuestionAndAnswer[0].a)
 let a = document.querySelector('.aLabel')
 let b = document.querySelector('.bLabel')
 let c = document.querySelector('.cLabel')
+let submit = document.getElementById('submit')
+let answers = document.querySelectorAll('.answer')
+
+
+function SubmitAnswer(){
+    submit.addEventListener('click',function(event){
+        event.preventDefault();
+
+        //getting answer from user
+        let result = getUserAnswer();
+
+        if(result == QuestionAndAnswer[0].answer ){
+            alert('Your answer is correct')
+        }
+        else{
+            alert("your answer is wrong")
+        }
+
+
+        
+        
+    })
+
+}
 
 
 
@@ -24,8 +48,19 @@ function Display(){
     b.innerHTML = QuestionAndAnswer[0].b;
     c.innerHTML = QuestionAndAnswer[0].c;
 }
+function getUserAnswer(){
+    let userAnswer;
+    answers.forEach(function(answer){
+        console.log(answer)
+        if(answer.checked == true){
+            userAnswer = answer.id
+        }
+    })
+    return userAnswer;
+}
 
 Display();
+SubmitAnswer();
 
 
 
